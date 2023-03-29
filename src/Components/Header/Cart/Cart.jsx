06e4228f3e55e7e9e004ecd6c8e082b99860691f
tanvirmdahmed <developertanvirahmed09@gmail.com>
2023-03-29 22:43:52 +0600

    first commit

---
+++ src/Components/Header/Cart/Cart.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({cart}) => {
+    // const cart = props.cart; // option 1
+    // const {cart} = props; // option 2
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+
+        // product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+
+    const tax = totalPrice * 7 / 100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
+    return (
+        <div className='cart'>
+            <h2 className='cart-heading'>Order Summary</h2>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
+        </div>
+    );
+};
+import './Cart.css'
+
+export default Cart;
---
+++ src/Components/Header/Product/Product.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
+const Product = (props) => {
+    const { img, name, seller, ratings, price } = props.product;
+    const handleAddCart = props.handleAddCart;
+   
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} Stars</p>
+            </div>
+            <button onClick={() => handleAddCart(props.product)} className='btn-cart'>Add to Cart <FontAwesomeIcon icon=   {faShoppingCart} />
+            </button>
+        </div>
+    );
+};
+
+export default Product;
